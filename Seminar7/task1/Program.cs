7edd89c6b9329b1a34f756521e485b7a965a284c
Pochechuev <trakturus@list.ru>
2023-09-06 17:55:09 +0300

    add task1

--- Seminar7/task1/Program.cs
+++ Seminar7/task1/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 46: Задайте двумерный массив размером m * n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+int[,] GetMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = rand.Next(minValue, maxValue + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int [,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    
+}
+
+int SetNumber(string message = "")
+{
+    System.Console.Write($"Введите число {message} : ");
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+int rows1 = SetNumber("rows");
+int columns1 = SetNumber("columns");
+int max = SetNumber("max");
+int min = SetNumber("min");
+
+int [,] matrix = GetMatrix(columns: columns1, rows: rows1, minValue: min, maxValue: max);
+PrintMatrix(matrix);
