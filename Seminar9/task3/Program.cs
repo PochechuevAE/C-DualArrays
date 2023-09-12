f145e52ff5d468d01fa4b78c37c1ce7525d93a80
Pochechuev <trakturus@list.ru>
2023-09-12 20:22:39 +0300

    wrote task3

--- Seminar9/task3/Program.cs
+++ Seminar9/task3/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿/*
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+System.Console.WriteLine(GetSum(453));
+
+int GetSum(int num)
+{
+    if (num == 0) 
+    {
+        return num;
+    }
+    
+    return num % 10 + GetSum(num / 10);
+}
