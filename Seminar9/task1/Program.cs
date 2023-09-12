f0fba546c3615f7081b7d9dc8ad957b38c042b7a
Pochechuev <trakturus@list.ru>
2023-09-12 20:14:35 +0300

    add task1

--- Seminar9/task1/Program.cs
+++ Seminar9/task1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+System.Console.WriteLine(PrintNumbers(1, 100));
+
+string PrintNumbers(int startNumber, int endNumber)
+{
+    if (startNumber == endNumber) 
+    {
+        return startNumber.ToString();
+    }
+    
+    return startNumber + " " + PrintNumbers(startNumber + 1, endNumber);
+}
