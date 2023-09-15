25d5f73b53722dd6087c488389d8eee25d69d170
Pochechuev <trakturus@list.ru>
2023-09-15 11:58:33 +0300

    wrote task4

--- Leson7/task4/Program.cs
+++ Leson7/task4/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿// Вычислить число a в степени n
+
+// Решение циклом
+
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= a;
+    }
+
+    return result;
+}
+
+//Решение рекурсией
+
+int PowerRec(int a, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    
+    else
+    {
+        return PowerRec(a, n -1) * a;
+    }
+}
+
+System.Console.WriteLine(PowerFor(2, 10));
+System.Console.WriteLine(PowerRec(2, 10));
