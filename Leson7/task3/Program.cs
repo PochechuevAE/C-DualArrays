9f31ecd55865d11353cdca5bcf9a8656671a7084
Pochechuev <trakturus@list.ru>
2023-09-15 11:48:23 +0300

    add task4

--- Leson7/task3/Program.cs
+++ Leson7/task3/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿//Факториал числа (произведение чисел от 1 до n)
+
+//Решение циклом
+
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {   
+        result *= i;
+    }
+
+    return result;
+}
+
+//Решение через рекурсию
+
+int FactorialRec(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    else
+    {
+        return n * FactorialRec(n-1);
+    }
+}
+
+System.Console.WriteLine(FactorialFor(10));
+System.Console.WriteLine(FactorialRec(10));
