e3581518ab5d3edd57651744c264ce781403121d
Pochechuev <trakturus@list.ru>
2023-09-15 12:10:36 +0300

    as

--- Leson7/task4/Program.cs
+++ Leson7/task4/Program.cs
@@ -28,5 +28,29 @@ int PowerRec(int a, int n)
     }
 }
 
+//Улучшение рекурсии
+
+int PowerRecMath(int a, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    else if (n % 2 == 0)
+    {
+        return PowerRecMath(a * a, n / 2);
+    }
+    
+    else
+    {
+        return PowerRecMath(a, n - 1) * a;
+    }
+}
+
+System.Console.WriteLine($"Цикл");
 System.Console.WriteLine(PowerFor(2, 10));
+System.Console.WriteLine($"Рекурсия");
 System.Console.WriteLine(PowerRec(2, 10));
+System.Console.WriteLine($"Улучшеная рекурсия");
+System.Console.WriteLine(PowerRecMath(2, 10));
