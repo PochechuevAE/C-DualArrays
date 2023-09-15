6e29055c3ad01ec7d546f324058ff2895d7ad621
Pochechuev <trakturus@list.ru>
2023-09-15 11:42:21 +0300

    wrote task2

--- Leson7/task2/Program.cs
+++ Leson7/task2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿// Сумма чисел от 1 до n
+
+//Решение циклом
+
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        result += i;
+    }
+
+    return result;
+}
+
+//Решение через рекурсию
+
+int SumRec(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return n + SumRec(n - 1);
+    }
+}
+
+System.Console.WriteLine(SumFor(10));
+System.Console.WriteLine(SumRec(10));
