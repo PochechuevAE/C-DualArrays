0d80f27644d9a68a9372eb60c58b552522e903ed
Pochechuev <trakturus@list.ru>
2023-09-15 13:22:58 +0300

    wrote task7

--- Leson7/task7/Program.cs
+++ Leson7/task7/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//Детская игра с башенками
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 5)
+{
+    if (count > 1)
+    {
+        Towers(with, some, on, count - 1);
+    }
+
+    System.Console.WriteLine($"{with} >> {on}");
+
+    if (count > 1)
+    {
+        Towers(some, on, with, count - 1);
+    }
+}
+
+Towers();
